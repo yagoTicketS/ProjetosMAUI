74b619464b932baf685b83fa63d725e2f388c605
yagoTicketS <124682798+yagoTicketS@users.noreply.github.com>
2024-11-05 16:04:45 -0300

    teste pr

--- AppTask/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/AppTask.AssemblyInfo.cs
+++ AppTask/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/AppTask.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppTask")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+80722ae9b70b609df607c21600de047ab5c936ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+6357875984f98596b6d477581543de6a1ef8b7c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppTask")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppTask")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
